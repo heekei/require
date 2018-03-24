8714cc627552bc5fea23b41ec549c9cd748c9293
Heekei <zxq0610@qq.com>
2018-03-25 01:03:31 +0800

    Update require.js

--- require.js
+++ require.js
@@ -1,7 +1,7 @@
 /**
  * require
  * @author Heekei
- * @version 0.2.1
+ * @version 0.2.0
  * 
  * @description 异步加载外部js,可回调
  * @param {Object} setting 配置项
@@ -17,35 +17,55 @@ var require = function (setting) {
     require[setting.url].Timer = null;
     // 加载依赖文件
     if (setting.depends) {
-        require[setting.url].dependsLoading = true; // 标记依赖文件正在加载
-        
-        for(var x in setting.depends){
-            if(require.loaded.indexOf(setting.depends[x]) !== -1){ // 检查依赖文件是否已经加载过
-                setting.depends.splice(x,1)
+        // 标记依赖文件正在加载
+        require[setting.url].dependsLoading = true;
+
+        // 检查依赖文件是否已经加载过
+        for (var x in setting.depends) {
+            if (require.loaded.indexOf(setting.depends[x]) !== -1) {
+                setting.depends.splice(x, 1)
             }
         }
-        require[setting.url].loadingDepends = setting.depends;// 记录正在加载的依赖到数组【加载中的依赖】
-        require[setting.url].dLen = setting.depends.length; // 设置主JS的依赖文件的长度
+
+        // 记录正在加载的依赖到数组【加载中的依赖】
+        require[setting.url].loadingDepends = setting.depends;
+
+        // 设置主JS的依赖文件的长度
+        require[setting.url].dLen = setting.depends.length;
+
+        // 定时检查依赖是否加载完毕
         require[setting.url].Timer = setInterval(function () {
-            if (require[setting.url].dependsLoading === false) { // 定时检查依赖是否加载完毕
-                require({ url: setting.url, callback: setting.callback }) // 加载主JS
+            if (require[setting.url].dependsLoading === false) { // 依赖文件加载完毕
+                // 加载主JS
+                require({
+                    url: setting.url,
+                    callback: setting.callback
+                })
                 clearInterval(require[setting.url].Timer) // 清除定时器
             }
         }, 0)
-        for (var d = 0; d < require[setting.url].dLen; d++) { // 循环加载依赖文件
+
+        // 循环加载依赖文件
+        for (var d = 0; d < require[setting.url].dLen; d++) {
             require({
                 url: setting.depends[d],
                 callback: function () {
-                    require[setting.url].loadingDepends.splice(require[setting.url].loadingDepends.indexOf(this.url), 1) // 加载完成后，将该依赖文件从【加载中的依赖】数组中删除
-                    if (require[setting.url].loadingDepends.length == 0) // 如果【加载中的依赖】为空
-                    { require[setting.url].dependsLoading = false; } // 将“依赖加载中”的状态标记为false
+                    // 加载完成后，将该依赖文件从【加载中的依赖】数组中删除
+                    require[setting.url].loadingDepends.splice(require[setting.url].loadingDepends.indexOf(this.url), 1)
+
+                    // 如果【加载中的依赖】为空，将“依赖加载中”的状态标记为false
+                    if (require[setting.url].loadingDepends.length == 0) require[setting.url].dependsLoading = false;
                 }
             })
 
         }
-        return; // 退出函数，保证主JS在加载依赖文件的过程中不进行加载
+
+        // 退出函数，保证主JS在加载依赖文件的过程中不进行加载
+        return;
     }
+
     var script = document.createElement("script"); // 加载JS文件
+
     // 触发回调
     if (script.readyState) { //IE 
         script.onreadystatechange = function () {
@@ -67,8 +87,12 @@ var require = function (setting) {
             }
         };
     }
+
     script.src = setting.url + "?" + Math.random();
-    if (setting.defer) { script.defer = "defer"; }
+
+    if (setting.defer) {
+        script.defer = "defer";
+    }
     require.loaded.push(setting.url);
     document.head.appendChild(script);
 }
